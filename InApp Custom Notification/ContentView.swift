9a94af197482f368d92ea3c225ed2f6fcfdcd93f
Kartikeya Saxena Jain <kartikeya.saxena.jain@gmail.com>
2023-10-03 10:44:29 -0400

    Initial Commit

---
+++ InApp Custom Notification/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InApp Custom Notification
+//
+//  Created by Kartikeya Saxena Jain on 10/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InApp Custom Notification/InApp_Custom_NotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  InApp_Custom_NotificationApp.swift
+//  InApp Custom Notification
+//
+//  Created by Kartikeya Saxena Jain on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct InApp_Custom_NotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
